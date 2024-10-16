b5cfd0e4f64af2b2eed9807be7d18f28597fe3f2
Xyrene Jade Torralba <182231457+xytorralba@users.noreply.github.com>
2024-10-16 23:19:18 +0800

    I added the variables and displays.

--- findhighestnumber_2nd.py
+++ findhighestnumber_2nd.py
@@ -1,4 +1,11 @@
 #DEFINE 5 VARIABLES
+print("Input five numbers please")
+num1 = int(input("1st number: "))
+num2 = int(input("2nd number: "))
+num3 = int(input("3rd number: "))
+num4 = int(input("4th number: "))
+num5 = int(input("5th number: "))
+
 #IF NUM1 IS GREATER THAN ALL
 #IF NUM2 IS GREATER THAN ALL
 #IF NUM3 IS GREATER THAN ALL
