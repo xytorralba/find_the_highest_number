56db14ae699fe34674111c09d93462f92f645802
Xyrene Jade Torralba <182231457+xytorralba@users.noreply.github.com>
2024-10-16 23:30:26 +0800

    The code is complete and ready for use.

--- findhighestnumber_2nd.py
+++ findhighestnumber_2nd.py
@@ -7,7 +7,21 @@ num4 = int(input("4th number: "))
 num5 = int(input("5th number: "))
 
 #IF NUM1 IS GREATER THAN ALL
+if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5:
+    print (f"Highest number is: {num1}")
+
 #IF NUM2 IS GREATER THAN ALL
+elif num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5:
+    print (f"Highest number is: {num2}")
+
 #IF NUM3 IS GREATER THAN ALL
+elif num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5:
+    print (f"Highest number is: {num3}")
+
 #IF NUM4 IS GREATER THAN ALL
+elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5:
+    print (f"Highest number is: {num4}")  
+
 #IF NUM5 IS GREATER THAN ALL
+else:
+    print (f"Highest number is: {num5}")  
