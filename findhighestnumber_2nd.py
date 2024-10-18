6dbb31d41ce4454a42d02f751500041cd18cfc6c
Xyrene Jade Torralba <182231457+xytorralba@users.noreply.github.com>
2024-10-19 03:01:13 +0800

    The code is complete and ready for use.

--- findhighestnumber_2nd.py
+++ findhighestnumber_2nd.py
@@ -7,25 +7,18 @@ num4 = int(input("4th number: "))
 num5 = int(input("5th number: "))
 
 #IF NUM1 IS GREATER THAN ALL
-if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5 and num1 != num2 and num1 != num3 and num1 != num4 and num1 != num5:
+if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5 and num1 == num2 or num1 == num3 or num1 == num4 or num1 == num5:
     print (f"Highest number is: {num1}")
-
-#IF NUM2 IS GREATER THAN ALL
-elif num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5 and num2 != num1 and num2 != num3 and num2 != num4 and num2 != num5:
-    print (f"Highest number is: {num2}")
-
 #IF NUM3 IS GREATER THAN ALL
-elif num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5 and num3 != num1 and num3 != num2 and num3 != num4 and num3 != num5:
+elif num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5 and num3 == num1 or num3 == num2 or num3 == num4 or num3 == num5:
     print (f"Highest number is: {num3}")
-
 #IF NUM4 IS GREATER THAN ALL
-elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5 and num4 != num1 and num4 != num2 and num4 != num3 and num4 != num5:
-    print (f"Highest number is: {num4}")  
-
+elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5 and num4 == num1 or num4 == num2 or num4 == num3 or num4 == num5:
+    print (f"Highest number is: {num4}") 
 #IF NUM5 IS GREATER THAN ALL
-elif num5 > num1 and num5 > num2 and num5 > num3 and num5 > num4 and num5 != num1 and num5 != num2 and num5 != num3 and num5 != num4:
+elif num5 > num1 and num5 > num2 and num5 > num3 and num5 > num4 and num5 == num1 or num5 == num2 or num5 == num3 or num5 == num4:
     print (f"Highest number is: {num5}")  
-
-#IF THERE ARE SAME HIGHEST NUMBER
+#IF NUM2 IS GREATER THAN ALL
 else:
-    print ("Highest number: None")
+    if num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5 and num2 == num1 or num2 == num3 or num2 == num4 or num5 == num5:
+        print (f"Highest number is: {num2}")
