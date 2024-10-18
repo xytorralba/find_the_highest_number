e5553e12c4beebdd3da3adc1a01c55b668057455
Xyrene Jade Torralba <182231457+xytorralba@users.noreply.github.com>
2024-10-18 21:37:05 +0800

    I added a condition that, if there are multiple numbers equal to the highest number, the output will indicate that there is no highest number.

--- findhighestnumber_2nd.py
+++ findhighestnumber_2nd.py
@@ -7,21 +7,25 @@ num4 = int(input("4th number: "))
 num5 = int(input("5th number: "))
 
 #IF NUM1 IS GREATER THAN ALL
-if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5:
+if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5 and num1 != num2 and num1 != num3 and num1 != num4 and num1 != num5:
     print (f"Highest number is: {num1}")
 
 #IF NUM2 IS GREATER THAN ALL
-elif num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5:
+elif num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5 and num2 != num1 and num2 != num3 and num2 != num4 and num2 != num5:
     print (f"Highest number is: {num2}")
 
 #IF NUM3 IS GREATER THAN ALL
-elif num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5:
+elif num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5 and num3 != num1 and num3 != num2 and num3 != num4 and num3 != num5:
     print (f"Highest number is: {num3}")
 
 #IF NUM4 IS GREATER THAN ALL
-elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5:
+elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5 and num4 != num1 and num4 != num2 and num4 != num3 and num4 != num5:
     print (f"Highest number is: {num4}")  
 
 #IF NUM5 IS GREATER THAN ALL
-else:
+elif num5 > num1 and num5 > num2 and num5 > num3 and num5 > num4 and num5 != num1 and num5 != num2 and num5 != num3 and num5 != num4:
     print (f"Highest number is: {num5}")  
+
+#IF THERE ARE SAME HIGHEST NUMBER
+else:
+    print ("Highest number: None")
