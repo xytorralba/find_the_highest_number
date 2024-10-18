b06138b55bc103f3647ad0d47e90db831453122b
Xyrene Jade Torralba <182231457+xytorralba@users.noreply.github.com>
2024-10-18 21:06:31 +0800

    I simplified the entire code when I realized there was an error in the original. The original code returns the first number if it is greater than the second number without checking the others, and only proceeds if the first number is less than the second.

--- findhighestnumber.py
+++ findhighestnumber.py
@@ -1,48 +1,24 @@
 #DEFINE 5 NUMBERS NAMING NUM1, NUM2, NUM3, NUM4, AND NUM5
-def find_highest(num1, num2, num3, num4, num5):
-    #IF NUM1 IS GREATER THAN NUM2
-    if num1 > num2:
-        #COMPARE NUM1 TO OTHER NUMBERS
-        if num1 > num3:
-            return num1
-        elif num1 > num4:
-            return num1
-        elif num1 > num5:
-            return num1
-        #IF NUM2 IS GREATER THAN NUM1
-        else:
+def find_highest(num1, num2, num3, num4, num5):  
+    #IF NUM1 IS GREATER THAN ALL NUMBERS
+    if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5:
+        return num1
+    #IF NUM3 IS GREATER THAN ALL NUMBERS
+    elif num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5:
+        return num3
+    #IF NUM4 IS GREATER THAN ALL NUMBERS
+    elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5:
+        return num4      
+    #IF NUM5 IS GREATER THAN ALL NUMBERS
+    elif num5 > num1 and num5 > num2 and num5 > num3 and num5 > num4:
+        return num5
+    #IF NUM2 IS GREATER THAN ALL NUMBERS:
+    else: 
+        if num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5:
             return num2
-  
-    #IF NUM2 IS GREATER THAN NUM3
-    elif num2 > num3:
-        #COMPARE NUM2 TO OTHER NUMBERS
-        if num2 > num4:
-            return num2
-        elif num2 > num5:
-            return num2
-        #IF NUM3 IS GREATER THAN NUM2
-        else:
-            return num3
-        
-    #IF NUM3 IS GREATER THAN NUM4
-    elif num3 > num4:
-        #COMPARE NUM3 TO OTHER NUMBERS
-        if num3 > num5:
-            return 3
-        #IF NUM4 IS GREATER THAN NUM3
-        else:
-            return num4
-    
-    else:
-        #IF NUM4 IS GREATER THAN NUM5
-        if num4 > num5:
-            return num4
-        #IF NUM5 IS GREATER THAN NUM4
-        else:
-            return num5
 
 #INPUT FIVE NUMBERS
-res = find_highest(12,16,-8,18,-84)
+res = find_highest(18,16,-8,18,-84)
 
 #PRINT THE HIGHEST NUMBER
 print(f"Highest number: {res}")
